134bafae5abdc44dedfe5761f943a36bfb510e0f
cadeberkeley <cade.richard@berkeley.edu>
2024-05-15 22:57:18 -0700

    material.h

---
+++ src/material.h
@@ -0,0 +1,21 @@
+#ifndef SCENE_OBJECT_H
+#define SCENE_OBJECT_H
+
+#include "vector.h"
+
+class Material {
+	public:
+		Material(float r, float g, float b, float a) {
+            this->r = r;
+            this->g = g;
+            this->b = b;
+            this->a = a;
+        }
+		Material(float r, float g, float b) : Material(r, g, b, 0.0) {}
+		Material(Vec3& c) : Material(c[0], c[1], c[2]) {}
+
+	private:
+		float r, g, b, a;
+};
+
+#endif
