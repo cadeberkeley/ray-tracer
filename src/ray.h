2386b2b4947b99243340f7af5bdfe0ed2113fe16
cadeberkeley <cade.richard@berkeley.edu>
2024-05-15 22:49:57 -0700

    ray.h

---
+++ src/ray.h
@@ -0,0 +1,29 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vector.h"
+
+class Ray {
+    public:
+        Vec3 origin, dir;
+        Vec3 color;
+
+        Ray(const Vec3& o, const Vec3& d) {
+            origin = o;
+            dir = (d-o).normalized();
+            color = newVec3(0.0, 0.0, 0.0);
+        }
+
+        Ray(const Vec3& o, const Vec3& d, const Vec3& c) {
+            origin = o;
+            dir = (d-o).normalized();
+            color = c;
+        }
+
+        Vec3 at(float t) {
+            return origin + t * dir;
+        }
+
+};
+
+#endif
